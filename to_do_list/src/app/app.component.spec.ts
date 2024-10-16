4b7739095a932ec078e52f6f445b075312efc1bd
khanyansa <khanyizwane.nsa@gmail.com>
2024-10-16 14:36:29 +0200

    added components

--- to_do_list/src/app/app.component.spec.ts
+++ to_do_list/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'project' title`, () => {
+  it(`should have the 'to_do_list' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project');
+    expect(app.title).toEqual('to_do_list');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, to_do_list');
   });
 });

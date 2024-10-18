323997b87e0804ec79d70f8364e860fa2ca33877
khanyansa <khanyizwane.nsa@gmail.com>
2024-10-18 10:59:32 +0200

    created a landing page

--- to_do_list/src/app/app.routes.ts
+++ to_do_list/src/app/app.routes.ts
@@ -1,33 +1,62 @@
+// import { Routes } from '@angular/router';
+// import { LoginSignupComponent } from './Pages/login-signup/login-signup.component';
+// import { LayoutComponent } from './Pages/layout/layout.component';
+// import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+
+// export const routes: Routes = [
+//     {
+//         path: '',
+//         redirectTo: 'loginsignup',
+//         pathMatch: 'full'
+//     },
+
+//     {
+//         path: 'loginsignup',
+//         component: LoginSignupComponent
+//     },
+//     {
+//         path: '',
+//         component: LayoutComponent,
+//         children:[
+//             {
+//                 path: 'dashboard',
+//                 component: DashboardComponent
+//             }
+//         ]
+//     }
+// ];
+
+
+
+// UPDATED CODE
 import { Routes } from '@angular/router';
 import { LoginSignupComponent } from './Pages/login-signup/login-signup.component';
 import { LayoutComponent } from './Pages/layout/layout.component';
 import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+import { LandingPageComponent } from './Pages/landing-page/landing-page.component';  
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'loginsignup',
-        pathMatch: 'full'
-    },
-
-    {
-        path: 'loginsignup',
-        component: LoginSignupComponent
-    },
-    {
-        path: '',
-        component: LayoutComponent,
-        children:[
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            }
-        ]
-    }
+  {
+    path: '',
+    redirectTo: 'landing',
+    pathMatch: 'full'
+  },
+  {
+    path: 'landing',
+    component: LandingPageComponent
+  },
+  {
+    path: 'loginsignup',
+    component: LoginSignupComponent
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ]
+  }
 ];
-
-
-// TO DO LIST CODE
-
-
-
